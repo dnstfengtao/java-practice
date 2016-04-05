f3c14a88ab40e6015407f5eccb38d67abc1db4e3
finleyfeng <fengjiantao@hotmail.com>
2016-04-05 18:04:02 +0800

    add new test for reference type

---
+++ src/main/java/com/dnst/common/IntegerCompare.java
@@ -0,0 +1,22 @@
+package com.dnst.common;
+
+/**
+ * @author Finley
+ */
+public class IntegerCompare {
+    public static void main(String[] args) {
+        Integer i1 = new Integer(1);
+        Integer i2 = new Integer(1);
+        if (i1 == i2) {
+            System.out.println("equals1");
+        }
+        if (i1.equals(i2)) {
+            System.out.println("sames.");
+        }
+        int i3 = 1;
+        int i4 = 1;
+        if (i3 == i4) {
+            System.out.println("equals2");
+        }
+    }
+}
