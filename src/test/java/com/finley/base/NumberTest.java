06d8755121e0428d1a9d55650f2523289a69efbc
fengjiantao <jiantao.feng@56qq.com>
2016-11-15 11:49:45 +0800

    add number test

---
+++ src/test/java/com/finley/base/NumberTest.java
@@ -0,0 +1,15 @@
+package com.finley.base;
+
+import org.junit.Test;
+
+/**
+ * @author fengjiantao.
+ * @since 2016/11/15
+ */
+public class NumberTest {
+
+    @Test
+    public void testMaxValue() {
+        System.out.println(String.valueOf(Long.MAX_VALUE));
+    }
+}
