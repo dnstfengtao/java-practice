0f8046e5f6f17867c8fb83b8f3ef298bad503e0a
fengjiantao <jiantao.feng@56qq.com>
2016-11-23 13:59:38 +0800

    MDC test

--- src/test/java/ch/qos/logback/LogBackTest.java
+++ src/test/java/ch/qos/logback/LogBackTest.java
@@ -1,8 +1,11 @@
 package ch.qos.logback;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 /**
  * @author fengjiantao.
@@ -14,6 +17,8 @@ public class LogBackTest {
 
     @Test
     public void testBasicLog() {
+        MDC.put("trackId", RandomStringUtils.randomAlphabetic(20).concat(StringUtils.SPACE));
         logger.debug("Test logback {}", "log back content.");
+        MDC.clear();
     }
 }
