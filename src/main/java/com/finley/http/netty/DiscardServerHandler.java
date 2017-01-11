00904d960fe7a0ef1ec393db57e720975bdbead3
finleyfeng <fengjiantao@hotmail.com>
2017-01-11 16:12:55 +0800

    test netty

---
+++ src/main/java/com/finley/http/netty/DiscardServerHandler.java
@@ -0,0 +1,24 @@
+package com.finley.http.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author fengjiantao.
+ * @since 1/11/17.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
--- src/test/java/org/apache/log4j/Log4j2Test.java
+++ src/test/java/org/apache/log4j/Log4j2Test.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
 import org.junit.Test;
+import org.slf4j.MDC;
 
 /**
  * @author fengjiantao.
@@ -33,6 +34,17 @@ public class Log4j2Test {
         }
         System.out.println(System.currentTimeMillis() - start);
         ThreadContext.clearAll();
+    }
+
+    @Test
+    public void testUsingMDC() {
+        MDC.put("trackID", RandomStringUtils.randomAlphabetic(20).concat(StringUtils.SPACE));
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+            logger.debug("test track id " + i);
+        }
+        System.out.println(System.currentTimeMillis() - start);
+        MDC.clear();
     }
 
 }
