676712353c975180d41dd85d79225eb66cea211d
finleyfeng <fengjiantao@hotmail.com>
2017-02-28 14:48:03 +0800

    test write

--- src/test/java/com/finley/base/nio/FileChannelTest.java
+++ src/test/java/com/finley/base/nio/FileChannelTest.java
@@ -5,7 +5,6 @@ import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.codec.CharEncoding;
@@ -18,14 +17,13 @@ import org.junit.Test;
 public class FileChannelTest {
 
     @Test
-    public void testBaseFileChannel() throws IOException {
+    public void testFileChannelReadData() throws IOException {
         String filePath = getClass().getClassLoader().getResource("file.txt").getPath();
-        System.out.println(filePath);
         final RandomAccessFile file = new RandomAccessFile(filePath, "rw");
         final FileChannel inChannel = file.getChannel();
         ByteBuffer byteBuffer = ByteBuffer.allocate(48);
 
-        int bytesRead = 0;
+        int bytesRead;
 
         List<Byte> byteList = new ArrayList<>();
         bytesRead = inChannel.read(byteBuffer);
@@ -48,6 +46,30 @@ public class FileChannelTest {
         System.out.println(new String(lines, CharEncoding.UTF_8));
 
         inChannel.close();
+        file.close();
+    }
+
+    @Test
+    public void testFileChannelWriteData() throws IOException {
+        String filePath = getClass().getClassLoader().getResource("file.txt").getPath();
+        final RandomAccessFile file = new RandomAccessFile(filePath, "rw");
+        final FileChannel outChannel = file.getChannel();
+
+        final String writeToFile = "New string to write to the file" + System.currentTimeMillis();
+        final ByteBuffer byteBuffer = ByteBuffer.allocate(48);
+
+        byteBuffer.clear();
+        byteBuffer.put(writeToFile.getBytes());
+
+        byteBuffer.flip();
+
+        outChannel.position(file.length());
+        while (byteBuffer.hasRemaining()) {
+            outChannel.write(byteBuffer);
+        }
+
+        outChannel.force(true);
+        outChannel.close();
         file.close();
     }
 
