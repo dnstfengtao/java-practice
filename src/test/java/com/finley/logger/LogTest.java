108f749295dbfc8ae37e05c15eda8b1cea301dc8
finleyfeng <fengjiantao@hotmail.com>
2017-04-10 09:34:43 +0800

    引入 logback

--- src/test/java/com/finley/logger/LogTest.java
+++ src/test/java/com/finley/logger/LogTest.java
@@ -2,19 +2,18 @@ package com.finley.logger;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.ThreadContext;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 /**
  * @author fengjiantao.
  * @since 2016/11/11
  */
-public class Log4j2Test {
+public class LogTest {
 
-    private final Logger logger = LogManager.getLogger();
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Test
     public void testBasicLogging() {
@@ -23,17 +22,6 @@ public class Log4j2Test {
             logger.debug("basic test");
         }
         System.out.println(System.currentTimeMillis() - start);
-    }
-
-    @Test
-    public void testUsingThreadContext() {
-        ThreadContext.put("trackID", RandomStringUtils.randomAlphabetic(20).concat(StringUtils.SPACE));
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            logger.debug("test track id " + i);
-        }
-        System.out.println(System.currentTimeMillis() - start);
-        ThreadContext.clearAll();
     }
 
     @Test
